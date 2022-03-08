857f9f9b078f14605b1bc09dcea127ea4d3e0c40
Kohei Watanabe <watanabe.kohei@gmail.com>
2022-03-08 22:46:57 +0900

    Update MAN

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
 #' Optimize the number of topics
 #'
-#' `divergence()` computes the divergence of topics to help users to find the
-#' optimal number of topics for LDA. Divergence is the average Kullback–Leibler
-#' distance between all the pairs of topic vectors in `x$phi`. The divergence
-#' score maximizes when the chosen number of topic `k` is optimal (Deveaud et
-#' al., 2014).
+#' These functions help users to find the optimal number of topics for LDA.
 #' @param x a LDA model fitted by [textmodel_seededlda()] or [textmodel_lda()]
+#' @details
+#' `divergence()` computes the average Kullback–Leibler distance between all the
+#' pairs of topic vectors in `x$phi`. The divergence score maximizes when the
+#' chosen number of topic `k` is optimal (Deveaud et al., 2014).
 #' @references Deveaud, Romain et al. (2014). "Accurate and Effective Latent
 #'   Concept Modeling for Ad Hoc Information Retrieval".
 #'   doi:10.3166/DN.17.1.61-84. *Document Numérique*.
@@ -14,6 +14,7 @@ divergence <- function(x) {
     UseMethod("divergence")
 }
 
+#' @importFrom proxyC dist
 #' @export
 divergence.textmodel_lda <- function(x) {
     div <- proxyC::dist(x$phi, method = "kullback")
