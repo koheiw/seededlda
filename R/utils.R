f720e7346b02d7adc10b963cee32d2bf4248a30e
Kohei Watanabe <watanabe.kohei@gmail.com>
2022-06-10 11:07:21 +0900

    Return the full matrix of divergence socres

--- R/utils.R
+++ R/utils.R
@@ -2,10 +2,10 @@
 #'
 #' These functions help users to find the optimal number of topics for LDA.
 #' @param x a LDA model fitted by [textmodel_seededlda()] or [textmodel_lda()]
-#' @details
-#' `divergence()` computes the average Kullback–Leibler distance between all the
-#' pairs of topic vectors in `x$phi`. The divergence score maximizes when the
-#' chosen number of topic `k` is optimal (Deveaud et al., 2014).
+#' @details `divergence()` computes the Kullback–Leibler distance between all
+#' the pairs of topic vectors in `x$phi`. The average divergence score should
+#' maximize when the chosen number of topic `k` is optimal (Deveaud et al.,
+#' 2014).
 #' @references Deveaud, Romain et al. (2014). "Accurate and Effective Latent
 #'   Concept Modeling for Ad Hoc Information Retrieval".
 #'   doi:10.3166/DN.17.1.61-84. *Document Numérique*.
@@ -19,5 +19,5 @@ divergence <- function(x) {
 divergence.textmodel_lda <- function(x) {
     div <- proxyC::dist(x$phi, method = "kullback")
     diag(div) <- NA
-    Matrix::mean(div, na.rm = TRUE)
+    return(div)
 }
