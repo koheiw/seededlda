f4623e23f59bf630c99b87e3199e2a8faba8b2ce
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-04-18 07:13:23 +0900

    Separate examples

--- R/lda.R
+++ R/lda.R
@@ -30,6 +30,24 @@
 #' @keywords textmodel
 #' @seealso [topicmodels][topicmodels::LDA]
 #' @export
+#' @examples
+#' \donttest{
+#' require(seededlda)
+#' require(quanteda)
+#'
+#' corp <- head(data_corpus_moviereviews, 500)
+#' toks <- tokens(corp, remove_punct = TRUE, remove_symbols = TRUE, remove_number = TRUE)
+#' dfmt <- dfm(toks) %>%
+#'     dfm_remove(stopwords('en'), min_nchar = 2) %>%
+#'     dfm_trim(min_termfreq = 0.90, termfreq_type = "quantile",
+#'              max_docfreq = 0.1, docfreq_type = "prop")
+#'
+#' lda <- textmodel_lda(head(dfmt, 450), 6)
+#' terms(lda)
+#' topics(lda)
+#' lda2 <- textmodel_lda(tail(dfmt, 50), model = lda) # new documents
+#' topics(lda2)
+#' }
 textmodel_lda <- function(
     x, k = 10, max_iter = 2000, alpha = 0.5, beta = 0.1, gamma = 0,
     model = NULL, verbose = quanteda_options("verbose")
