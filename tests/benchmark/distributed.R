f15b017cd0340b82257486870faaeb3703f8d2e5
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-04-25 10:51:32 +0900

    Update tests

--- tests/benchmark/distributed.R
+++ tests/benchmark/distributed.R
@@ -14,7 +14,6 @@ dfmt <- dfm(toks) %>%
 
 lda1 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.1)
 lda2 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.01)
-lda3 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200)
 
 microbenchmark::microbenchmark(
     para = textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 1000, batch_size = 0.2),
@@ -24,3 +23,15 @@ microbenchmark::microbenchmark(
 
 terms(lda1)
 table(topics(lda1))
+
+lda1 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 100)
+lda2 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 100, model = lda1)
+lda3 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 800, model = lda2)
+
+terms(lda1)
+terms(lda2)
+
+dist <- proxyC::dist(lda1$phi, lda2$phi, method = "jensen")
+Matrix::diag(dist)
+dist <- proxyC::dist(lda1$phi, lda3$phi, method = "jensen")
+Matrix::diag(dist)
