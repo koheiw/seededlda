3e46d5bc6af926e44c67fe84cab20ccb9a5b84b3
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-05-22 12:28:01 +0900

    Update

--- tests/benchmark/distributed.R
+++ tests/benchmark/distributed.R
@@ -1,8 +1,9 @@
 require(seededlda)
 require(quanteda)
 
-#data_corpus_guardian <- readRDS('/home/kohei/Dropbox/Public/data_corpus_guardian2016-10k.rds')
-data_corpus_guardian <- readRDS('C:/Users/watan/Dropbox/Public/data_corpus_guardian2016-10k.rds')
+data_corpus_guardian <- readRDS('../Dropbox/Public/data_corpus_guardian2016.rds')
+data_corpus_guardian <- readRDS('/home/kohei/Dropbox/Public/data_corpus_guardian2016-10k.rds')
+#data_corpus_guardian <- readRDS('C:/Users/watan/Dropbox/Public/data_corpus_guardian2016-10k.rds')
 
 toks <- tokens(data_corpus_guardian,
                remove_punct = TRUE,
@@ -12,9 +13,11 @@ dfmt <- dfm(toks) %>%
     dfm_remove(stopwords(), min_nchar = 2) %>%
     dfm_trim(max_docfreq = 0.1, docfreq_type = "prop")
 
+lda0 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 1)
 lda1 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.5)
 lda2 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.2)
 lda3 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.1)
+lda4 <- textmodel_lda(dfmt, k = 20, verbose = TRUE, max_iter = 200, batch_size = 0.05)
 
 microbenchmark::microbenchmark(
     para = textmodel_lda(dfmt, k = 20, verbose = TRUE, batch_size = 0.01),
