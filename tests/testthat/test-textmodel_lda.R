03410df0f738d29baf1ad8c891197d40ee0fb793
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-08-22 11:46:43 +0900

    Update tests

--- tests/testthat/test-textmodel_lda.R
+++ tests/testthat/test-textmodel_lda.R
@@ -72,8 +72,8 @@ test_that("LDA is working", {
     )
     expect_equal(
         names(lda),
-        c("k", "max_iter", "last_iter", "auto_iter", "alpha", "beta", "gamma", "phi", "theta",
-          "words", "data", "batch_size", "call", "version")
+        c("k", "max_iter", "last_iter", "auto_iter", "alpha", "beta", "epsilon", "gamma",
+          "phi", "theta", "words", "data", "batch_size", "call", "version")
     )
     expect_equal(lda$last_iter, 200)
     expect_equivalent(class(lda$words), "dgCMatrix")
@@ -122,6 +122,7 @@ test_that("adjust_alpha works", {
 	lda <- textmodel_lda(dfmt, max_iter = 200, adjust_alpha = 0.5)
 	expect_true(all(lda$alpha != 0.5))
 	expect_true(all(lda$alpha > 0.25))
+	expect_true(all(lda$epsilon > 0))
 
 	expect_error(
 		textmodel_lda(dfmt, max_iter = 200, adjust_alpha = 1.5),
